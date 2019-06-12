7d715f7d4826d89865f53db60b967ac2978488c3
Matty <mathewpalumbo86@gmail.com>
2019-06-12 23:15:44 +0800

    setup terrain movement prototype
    - setup terrain movement prototype scene
    - terrain manager script controls global values for terrain movement speed, life length and instantiates terrain prefabs from the terrain spawner object
     - terrain movement behaviour controls each instanced terrain prefabs movement and destroys at the end of their life
    - added a couple of mats
    - added a couple of prefabs
    - updated some project settings (tags)

---
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/TerrainManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+
+    // Terrain prefab to be instantiated.
+    public GameObject terrainPrefab;
+
+    // How fast the terrain moves towards the player.
+    public float terrainSpeed;
+
+    // Life length in seconds of the terrain.
+    public float terrainLife;
+
+    // Timing values for instantiation.
+    public float waitToStart;
+    public float waitForNextTerrain;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // TimedTerrainPlacement will be called after a delay, and then repeat after a different delay.
+        InvokeRepeating("TimedTerrainPlacement", waitToStart, waitForNextTerrain);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Instantiate the terrain prefabs when called.
+     public void TimedTerrainPlacement()
+    {
+        Instantiate(terrainPrefab, this.gameObject.transform.position, this.gameObject.transform.rotation);
+    } 
+
+}
---
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/TerrainMovementBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainMovementBehaviour : MonoBehaviour
+{
+
+    // Movement speed of this terrain instance.
+    public float terrainMovementSpeed;
+
+    // Terrain life length of this instance.
+    public float thisTerrainLife;
+
+    // Stores a reference to the terrain manager.
+    private GameObject theTerrainManager;
+    
+    // Called when terrain instantiates.
+    void Awake()
+    {
+        // Find the terrain spawner with tag.
+        theTerrainManager = GameObject.FindGameObjectWithTag("TerrainSpawner");
+
+        // Access the terrain manager script on the spawner and set the speed.
+        terrainMovementSpeed = theTerrainManager.gameObject.GetComponent<TerrainManager>().terrainSpeed;
+
+        // Access the terrain manager script and set the life length.
+        thisTerrainLife = theTerrainManager.gameObject.GetComponent<TerrainManager>().terrainLife;
+
+        // Start coroutine determining life of each instance
+        StartCoroutine(LifeOfTerrainInstance());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move this instance of terrain.
+        transform.Translate(Vector3.back * Time.deltaTime * terrainMovementSpeed);
+    }
+
+    IEnumerator LifeOfTerrainInstance()
+    {
+        // Wait for the life length of terrain then destroy it.
+        yield return new WaitForSeconds(thisTerrainLife);
+        Destroy(this.gameObject);
+    }
+
+}
