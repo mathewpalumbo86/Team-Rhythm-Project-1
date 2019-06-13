c17a00063867cacc6d6a0da40ad31cda4988b125
Matty <mathewpalumbo86@gmail.com>
2019-06-13 22:08:21 +0800

    updated movement prototyping
    - updated some scripts
    - swapped the camera to the OVR player controller and updated the prefab

---
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/PlayerMovement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Reference to the controller
+    public GameObject controllerGO;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       // controllerGO = OVRInput.Controller.RTrackedRemote 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
