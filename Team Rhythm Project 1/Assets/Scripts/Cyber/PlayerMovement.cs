e6bd08833fbce8e7341e5fbf0eb2fb120b0f6bf6
Matty <mathewpalumbo86@gmail.com>
2019-06-16 12:23:48 +0800

    setup moving terrrain and controller rotation UI
    - added temp terrain mesh
    - added mesh contrainer prefab (not used)
    - added controller rotation UI
    - updated player movement script (not finished)

--- Team Rhythm Project 1/Assets/Scripts/Cyber/PlayerMovement.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/PlayerMovement.cs
@@ -1,22 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Reference to the controller
-    public GameObject controllerGO;
-    
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-       // controllerGO = OVRInput.Controller.RTrackedRemote 
-    }
+    // Will store the controller rotation
+    public Quaternion controllerGO;
+
+    // Controller values shown in UI overlay
+    public Text xText;
+    public Text yText;
+    public Text zText;
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Get the controller rotation
+        controllerGO = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+
+        // Converts the quaternion to readable values
+        Vector3 angles = controllerGO.eulerAngles;
+
+        // Updaating the text overlay
+        xText.text = "X: " + angles.x;
+        yText.text = "Y: " + angles.y; 
+        zText.text = "Z: " + angles.z; 
+
+
+        // xText.text = controllerGO.rotation.eulerAngles.x;
+
+
+
+
+
+        // Convert rotation to either a left, right or middle position?
+        // Restrictions:
+        // can only move in a straight line between 2 points
+        // can move either way or pause in a position
+        // 
+
+
+        // Update the players position
+
+
+
+
     }
 }
