f668adf1de0003fac55c2ca830732e2f6f1a87d5
Matty <mathewpalumbo86@gmail.com>
2019-06-16 19:13:25 +0800

    basic player movement working
    - player movement script functional (player can move left and right between 2 points)
    - updated scene and script

--- Team Rhythm Project 1/Assets/Scripts/Cyber/PlayerMovement.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/PlayerMovement.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+// To be attached to the player vehicle.
+
 public class PlayerMovement : MonoBehaviour
 {
     // Will store the controller rotation
@@ -12,36 +14,72 @@ public class PlayerMovement : MonoBehaviour
     public Text xText;
     public Text yText;
     public Text zText;
+    // Used to indicate if the controller is pointing forward, left or right.
+    public Text controllerDirectionText;
+
+    // Movement targets
+    public Transform leftTarget;
+    public Transform rightTarget;
+
+
+    // The speed the vehicle should move.
+    public float speed = 1.0f;
+
+
+    void Start()
+    {
+         
+    }
 
     // Update is called once per frame
     void Update()
     {
+        // calculate distance to move
+        float step = speed * Time.deltaTime; 
+
+
+
         // Get the controller rotation
         controllerGO = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
 
         // Converts the quaternion to readable values
         Vector3 angles = controllerGO.eulerAngles;
 
-        // Updaating the text overlay
+        // Updating the text overlay
         xText.text = "X: " + angles.x;
         yText.text = "Y: " + angles.y; 
-        zText.text = "Z: " + angles.z; 
+        zText.text = "Z: " + angles.z;
+
+        // Checks the controller y rotation and assigns it a direction (forward, left, right). Moves the player vehicle accordingly.
+        if ((angles.y >=315 && angles.y<= 360) ||(angles.y >= 0 && angles.y <= 45))
+        {
+            controllerDirectionText.text = "Direction: Forward";
+
+            // Vehicle doesn't move when controller points forwards.
+
+        }
+        else if((angles.y >= 180 && angles.y <= 315))
+        {
+            controllerDirectionText.text = "Direction: Left";
+
+            // (x-)
+            // Move our position a step closer to the target.            
+            transform.position = Vector3.MoveTowards(transform.position, leftTarget.position, step);
 
 
-        // xText.text = controllerGO.rotation.eulerAngles.x;
+        }
+        else if ((angles.y >= 45 && angles.y <= 180))
+        {
+            controllerDirectionText.text = "Direction: Right";
+
+            // (x+)
+            // Move our position a step closer to the target.            
+            transform.position = Vector3.MoveTowards(transform.position, rightTarget.position, step);
 
 
+        }
 
 
-
-        // Convert rotation to either a left, right or middle position?
-        // Restrictions:
-        // can only move in a straight line between 2 points
-        // can move either way or pause in a position
-        // 
-
-
-        // Update the players position
 
 
 
