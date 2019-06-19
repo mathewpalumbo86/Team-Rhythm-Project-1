26cb47922b2fbcd74dc41accf0bd6093161a50d3
Matty <mathewpalumbo86@gmail.com>
2019-06-19 22:35:50 +0800

    Revert "working on object pooling - not finished"
    This reverts commit 6fe018d050940249c3d1fd337a6b956723804f7d.

--- Team Rhythm Project 1/Assets/Scripts/Cyber/TerrainMovementBehaviour.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/TerrainMovementBehaviour.cs
@@ -7,20 +7,27 @@ public class TerrainMovementBehaviour : MonoBehaviour
 
     // Movement speed of this terrain instance.
     public float terrainMovementSpeed;
-        
+
+    // Terrain life length of this instance.
+    public float thisTerrainLife;
+
     // Stores a reference to the terrain manager.
     private GameObject theTerrainManager;
     
-    // Called when terrain object is turned on.
+    // Called when terrain instantiates.
     void OnEnable ()
     {
         // Find the terrain spawner with tag.
         theTerrainManager = GameObject.FindGameObjectWithTag("TerrainSpawner");
 
         // Access the terrain manager script on the spawner and set the speed.
-        terrainMovementSpeed = theTerrainManager.gameObject.GetComponent<TerrainManager>().terrainSpeed;        
+        terrainMovementSpeed = theTerrainManager.gameObject.GetComponent<TerrainManager>().terrainSpeed;
 
-       
+        // Access the terrain manager script and set the life length.
+        thisTerrainLife = theTerrainManager.gameObject.GetComponent<TerrainManager>().terrainLife;
+
+        // Start coroutine determining life of each instance
+        StartCoroutine(LifeOfTerrainInstance());
 
     }
 
@@ -31,7 +38,11 @@ public class TerrainMovementBehaviour : MonoBehaviour
         transform.Translate(Vector3.back * Time.deltaTime * terrainMovementSpeed);
     }
 
-    
-
+    IEnumerator LifeOfTerrainInstance()
+    {
+        // Wait for the life length of terrain then destroy it.
+        yield return new WaitForSeconds(thisTerrainLife);
+        Destroy(this.gameObject);
+    }
 
 }
