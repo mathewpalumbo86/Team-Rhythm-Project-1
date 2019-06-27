8f54fee54f7c78c2a5a633500bd7d0b2957d5dbc
Matty <mathewpalumbo86@gmail.com>
2019-06-27 19:52:56 +0800

    Merge pull request #20 from mathewpalumbo86/Matty
    added audio effects and the PlayCollisionSound script

---
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/PlayCollisionSound.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//attach this script to any object that will play a sound
+public class PlayCollisionSound : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource[] soundEffects; // reference to the audio sources that are attached to the audio manager
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //checks if the collision is coming from the player, might want to swap this to an OnTriggerEnter.
+        if (collision.collider.tag == "Player")
+        {
+            int soundToPlay = RandomiseSoundEffect();
+            soundEffects[soundToPlay].PlayOneShot(soundEffects[soundToPlay].clip); // plays the sound once on collsion
+        }
+    }
+
+    //randomises the sound to play from the array
+    int RandomiseSoundEffect()
+    {
+        int chooseSound = Random.Range(0, (soundEffects.Length - 1)); // minus one because using just the length of the array will cause an out of bounds error
+        return chooseSound;
+    }
+}
