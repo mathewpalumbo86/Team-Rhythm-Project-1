fa3f1751ab7de5bef425f8355d1ef394d021cff8
Lochlan Scott <38372089+Pharah-tato@users.noreply.github.com>
2019-06-27 19:09:43 +0800

    Added New scene for testing

---
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/Object Collisions/PlayCollisionSound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//attach this script to any object that will play a sound
+[RequireComponent(typeof(Collider))]
+public class PlayCollisionSound : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource soundEffect; // reference to the audio source that's attached to the audio manager
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        //checks if the collision is coming from the player, might want to swap this to an OnTriggerEnter.
+        if (collision.collider.tag == "player")
+        {
+            soundEffect.PlayOneShot(soundEffect.clip); // plays the sound once on collsion
+            
+        }
+    }
+}
