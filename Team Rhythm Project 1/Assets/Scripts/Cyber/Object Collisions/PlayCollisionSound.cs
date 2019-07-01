317ec1372155fd4012f29a734cdf2e93f59ea67e
L. Scott <38372089+LaserDawn84@users.noreply.github.com>
2019-07-01 15:55:47 +0800

    Update PlayCollisionSound.cs

--- Team Rhythm Project 1/Assets/Scripts/Cyber/Object Collisions/PlayCollisionSound.cs
+++ Team Rhythm Project 1/Assets/Scripts/Cyber/Object Collisions/PlayCollisionSound.cs
@@ -3,19 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //attach this script to any object that will play a sound
-[RequireComponent(typeof(Collider))]
 public class PlayCollisionSound : MonoBehaviour
 {
     [SerializeField]
-    AudioSource soundEffect; // reference to the audio source that's attached to the audio manager
+    AudioSource[] soundEffects; // reference to the audio sources that are attached to the audio manager
 
     private void OnCollisionEnter(Collision collision)
     {
         //checks if the collision is coming from the player, might want to swap this to an OnTriggerEnter.
-        if (collision.collider.tag == "player")
+        if (collision.collider.tag == "Player")
         {
-            soundEffect.PlayOneShot(soundEffect.clip); // plays the sound once on collsion
-            
+            int soundToPlay = RandomiseSoundEffect();
+            soundEffects[soundToPlay].PlayOneShot(soundEffects[soundToPlay].clip); // plays the sound once on collsion
         }
+    }
+
+    //randomises the sound to play from the array
+    int RandomiseSoundEffect()
+    {
+        int chooseSound = Random.Range(0, (soundEffects.Length - 1)); // minus one because using just the length of the array will cause an out of bounds error
+        return chooseSound;
     }
 }
